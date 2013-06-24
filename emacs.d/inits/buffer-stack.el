4577f3488d05868bce1c1b65815e90c8c0cd4410
Erhart, Steven <erhart@email.unc.edu>
2013-06-24 10:08:16 -0400

    removed customize variables and moved the buffer-stack once to its init file

---
+++ emacs.d/inits/buffer-stack.el
@@ -0,0 +1,2 @@
+(customize-set-variable
+ '(buffer-stack-untracked (quote ("KILL" "*Compile-Log*" "*Compile-Log-Show*" "*Group*" "*Completions*" "*Messages*" "*magit-process*" "*magit-commit*" "*Ediff Registry*"))))

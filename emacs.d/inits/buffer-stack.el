0cf8ab2ebc4df3dfb45f0569c80f33cf8be4bf8a
Steve Erhart <erhart@email.unc.edu>
2013-08-08 12:01:16 -0400

    added buffer stack init

--- emacs.d/inits/buffer-stack.el
+++ emacs.d/inits/buffer-stack.el
@@ -1,2 +1,8 @@
-(customize-set-variable
- '(buffer-stack-untracked (quote ("KILL" "*Compile-Log*" "*Compile-Log-Show*" "*Group*" "*Completions*" "*Messages*" "*magit-process*" "*magit-commit*" "*Ediff Registry*"))))
+(customize-set-variable 'buffer-stack-untracked (quote ("KILL" 
+                                                        "*Compile-Log*" "*Compile-Log-Show*"
+                                                        "*Group*" 
+                                                        "*Completions*" 
+                                                        "*Messages*" 
+                                                        "*Packages*"
+                                                        "*magit-process*" "*magit-commit*" "*magit-edit-log"
+                                                        "*Ediff Registry*")))

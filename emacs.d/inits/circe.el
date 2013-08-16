da1a856abcd147313847b634bd87ef0f222b5345
Steve Erhart <erhart@aalmusic01.lib.unc.edu>
2013-08-16 15:26:10 -0400

    better chat config

--- emacs.d/inits/circe.el
+++ emacs.d/inits/circe.el
@@ -1,13 +1,15 @@
 (when (file-exists-p "~/.private.el")
   (load-file "~/.private.el"))
 
-(setq circe-network-options
-      `(("bbb"
-         :host "bonerbonerboner.com"
-         :tls t
-         :port 7667
-         :nick "serhart"
-         :reduce-lurker-spam t
-         :user "steve"
-         :pass ,bbb-password ; set in ~/.private.el
-         )))
+(setq circe-network-options `(("bbb"
+                               :host "bonerbonerboner.com"
+                               :tls t
+                               :port 7667
+                               :nick "serhart"
+                               :reduce-lurker-spam t
+                               :user "steve"
+                               :pass ,bbb-password ; set in ~/.private.el
+                               ))
+      lui-flyspell-p t
+      lui-max-buffer-size 30000
+      lui-flyspell-alist '((".*" "american")))
--- emacs.d/inits/jabber.el
+++ emacs.d/inits/jabber.el
@@ -1,10 +1,14 @@
+(require 'jabber)
+
+(when (file-exists-p "~/.private.el")
+  (load-file "~/.private.el"))
+
 (setq
- jabber-account-list '(("steveerhart@bonerbonerboner.com/emacs"
-                        (:port . 5222)
-                        (:connection-type . starttls)))
- jabber-roster-line-format "%n"
+ jabber-account-list `(("steveerhart@bonerbonerboner.com/emacs" (:port . 5222) (:password . jabber-password) (:connection-type . starttls)))
+ jabber-roster-line-format "%a %n"
  jabber-roster-show-title nil
  jabber-auto-reconnect t
  jabber-show-offline-contacts nil
+ jabber-debug-log-xml t
  jabber-chatstates-confirm nil
  jabber-backlog-number 30)

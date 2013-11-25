98e50abd80e62133de68dfeb026fba24e6ee7147
Steve Erhart <steve.erhart@gmail.com>
2013-11-25 10:17:40 -0500

    change opacity

--- emacs.d/display.el
+++ emacs.d/display.el
@@ -16,8 +16,8 @@
 
   (setq ns-use-native-fullscreen nil)
   (set-frame-parameter nil 'fullscreen 'fullboth)
-  (set-frame-parameter (selected-frame) 'alpha '(70 50))
-  (add-to-list 'default-frame-alist '(alpha 70 70))
+  (set-frame-parameter (selected-frame) 'alpha '(90 50))
+  (add-to-list 'default-frame-alist '(alpha 80 70))
 
   (global-linum-mode 1)
   (blink-cursor-mode 0)

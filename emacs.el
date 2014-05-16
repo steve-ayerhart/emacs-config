fab37d28f78caa18e88fa32e9a819e6c298e3370
Steve Erhart <erhart@aalmusic01.lib.unc.edu>
2014-05-16 15:45:44 -0400

    added zenburn init

--- emacs.el
+++ emacs.el
@@ -66,3 +66,15 @@ tramp-verbose 9
 
 (global-set-key (kbd "M-ESC") 'jump-to-end)
 (global-set-key (kbd "<f1>") 'magit-status)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes (quote ("cd70962b469931807533f5ab78293e901253f5eeb133a46c2965359f23bfb2ea" default))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:family "Inconsolata" :foundry "nil" :slant normal :weight normal :height 140 :width normal)))))

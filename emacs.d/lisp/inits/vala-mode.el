72f6349677ac8d1374b1f101a335a3f6ae3829f8
Steve Erhart <erhart@email.unc.edu>
2014-07-21 19:42:44 -0400

    vala-mode init

---
+++ emacs.d/lisp/inits/vala-mode.el
@@ -0,0 +1,4 @@
+(add-to-list 'auto-mode-alist '("\\.vala$" . vala-mode))
+(add-to-list 'auto-mode-alist '("\\.vapi$" . vala-mode))
+(add-to-list 'file-coding-system-alist '("\\.vala$" . utf-8))
+(add-to-list 'file-coding-system-alist '("\\.vapi$" . utf-8))

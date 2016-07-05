810f8be0e2285d609a66cf12efc1ac7fecf63145
Steve Erhart (erhart) <erhart@email.unc.edu>
2016-07-05 14:16:26 -0400

    powerline

--- emacs.d/lisp/inits/powerline.el
+++ emacs.d/lisp/inits/powerline.el
@@ -1,7 +1,4 @@
 (require 'powerline)
 
-(setq powerline-arrow-shape 'arrow)
 
-(custom-set-faces
- '(mode-line ((t (:foreground "#040404" :background "#bdbdbd" :box nil))))
- '(mode-line-inactive ((t (:foreground "#f9f9f9" :background "#666666" :box nil)))))
+(powerline-center-evil-theme)
